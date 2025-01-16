8e53fac5f25bf8d1f88e72c086b7d081b7206fa8
Irina Chirita <chirita@adobe.com>
2025-01-16 14:44:48 +0000

    Completed Day 2 Assignments

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -2,4 +2,6 @@ export default function decorate(block) {
     const contentWrapper = block.firstElementChild;
     const pic = contentWrapper.querySelector('img')
     pic.classList.add('banner');
+    const paragraph = contentWrapper.querySelector('p')
+    paragraph.classList.add('banner-text');
 }
