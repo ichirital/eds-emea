de11a4a7958f7a0ec1764d5498c47cb8ad22d132
Irina Chirita <chirita@adobe.com>
2025-01-28 16:59:44 +0000

    build issues

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,10 @@
-import { fetchPlaceholders } from
-'../../scripts/aem.js';
+import { fetchPlaceholders } 
+from '../../scripts/aem.js';
+
 export default async function decorate(block) {
     const placeholders = await fetchPlaceholders();
+    const {more, stuff} = placeholders;
+    window.console.log("more: " + more);
+    window.console.log("stuff: " + stuff);
+    
 }
